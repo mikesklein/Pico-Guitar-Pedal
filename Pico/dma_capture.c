06a8c379e430dcdc4ec606163168caabb2951e84
Michael <mike@michaelklein.ca>
2021-02-25 17:24:57 -0600

    Reverted to Time Based Implementation
    * using timers and interrupts
    * multi core use

--- Pico/dma_capture.c
+++ Pico/dma_capture.c
@@ -9,27 +9,27 @@
 
 
 #define CAPTURE_CHANNEL 0
-#define CAPTURE_DEPTH 132300
-#define FLAG_VALUE1 123
-#define FLAG_VALUE2 321
+// #define CAPTURE_DEPTH 132300
 
-uint8_t capture_buf[CAPTURE_DEPTH];
-static int core0_rx_val = 0, core1_rx_val = 0;
-static uint16_t adc_value = 0;
-static uint16_t adc_value_core1 = 0;
+// uint8_t capture_buf[CAPTURE_DEPTH];
+const uint LED_PIN = 16;
 
 void core1_main();
 
+
+
 bool read_adc_timer(struct repeating_timer *t) {
-    adc_value = adc_read();
+    uint16_t result = adc_read();
     if (multicore_fifo_wready){
-        multicore_fifo_push_blocking(adc_value);
+        multicore_fifo_push_blocking(result);
     }
     return true;
 }
 
 int main() {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     multicore_launch_core1(core1_main);
 
     adc_gpio_init(26 + CAPTURE_CHANNEL);
@@ -38,26 +38,40 @@ int main() {
     adc_set_clkdiv(0);
 
     struct repeating_timer timer;
-    
-    add_repeating_timer_ms(0.01, read_adc_timer, NULL, &timer);
+    add_repeating_timer_ms(1, read_adc_timer, NULL, &timer);
 
     while (true) {
-     //   printf("Hello from core 0 \n");
-        
-      //  sleep_ms(1000);
+     tight_loop_contents();
     }
+}
+
+void core1_interrupt_handler() {
+
+    // Receive Raw Value, Convert and Print Temperature Value
+    while (multicore_fifo_rvalid()){
+        uint16_t raw = multicore_fifo_pop_blocking();
+       // const float conversion_factor = 3.3f / (1 << 12);
+        gpio_put(LED_PIN, 1);
+        printf("%-3d\n", raw);
+        gpio_put(LED_PIN, 0);
+
+    }
+
+    multicore_fifo_clear_irq(); // Clear interrupt
 }
 
 void core1_main() {
-    
-    while (true) {
-       if (multicore_fifo_rvalid){
-            adc_value_core1 = multicore_fifo_pop_blocking();
-            printf("%-3d\n", adc_value_core1);
-       }
+// Configure Core 1 Interrupt
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-        
+    multicore_fifo_clear_irq();
+    irq_set_exclusive_handler(SIO_IRQ_PROC1, core1_interrupt_handler);
+    irq_set_enabled(SIO_IRQ_PROC1, true);
 
-      //  sleep_ms(1000);
+    // Infinte While Loop to wait for interrupt
+    while (1){
+        tight_loop_contents();
     }
 }
+
